4c70d7afecba57c6ee88f097e40bc8f415470ba4
TheFallen001 <167517705+TheFallen001@users.noreply.github.com>
2024-10-22 15:36:16 +0300

    added functions

---
+++ Main.java
@@ -0,0 +1,29 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main( ) {
+
+        
+        
+    }
+
+    public static void statistics()
+    {
+
+    }
+
+    public static void matrix()
+    {
+        
+    }
+    public static void passcode()
+    {
+        
+    }
+    public static void tictactoe()
+    {
+        
+    }
+}
