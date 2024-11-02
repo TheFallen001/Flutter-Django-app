69b913168aa140c94b26292d83dfcefe3453d757
TheFallen001 <ustazabdulfattah28@gmail.com>
2024-11-02 15:46:16 +0300

    rthtgrfa

---
+++ ascii_art.java
@@ -0,0 +1,10 @@
+public class ascii_art {
+    private static char[][] W = new char[6][6];
+  
+    public static void combine(String[] args){
+        
+        for (int i = 0; i < args.length; i++) {
+            ans +=
+        }
+    }
+}
